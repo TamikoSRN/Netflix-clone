b242fbe1d49ef272b7a39fab88f10a2543748fd9
TamikoSRN <thomasereno1@gmail.com>
2022-04-28 03:34:15 -0300

    Homepage finished, bugs to fix with the cards

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,21 +1,49 @@
-import { Add, PlayArrow, ThumbDownOutlined, ThumbUpOutlined } from "@material-ui/icons";
+import { useState } from "react";
+import {
+  Add,
+  PlayArrow,
+  ThumbDownOutlined,
+  ThumbUpOutlined,
+} from "@material-ui/icons";
 import "./ListItem.scss";
 
-const ListItem = () => {
+const ListItem = ({ index }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <div className="listItem">
+    <div
+      className="listItem"
+      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <img
-        src="https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABWBSIsuEE08Fly6ugJ4krgHGBlajIpZ8jliwQ1QR8jvyZbNLkq3UXLLVzyIJdoED_xNum5pEDY9R6bWTjeWs-2ZdgZvpfs3bgK4VPfigQtJMVnWMDSg38dHusm-cfQ.jpg?r=5d0"
+        src="https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABTyDAmzRmRNYNP1o8GGL9apQQz0sAZ19m600IkGZb0q4mIuAZyXGeu1weOuNnTbHSTp5fazXEr8L-52ZJgG-x47nVrNF.jpg?r=6b0"
         alt=""
       />
-      <div className="itemInfo">
-        <div className="icons">
-          <PlayArrow />
-          <Add />
-          <ThumbUpOutlined />
-          <ThumbDownOutlined />
-        </div>
-      </div>
+      {isHovered && (
+        <>
+          <video src="https://youtu.be/GTA20VJeyYk" autoPlay={true} loop />
+          <div className="itemInfo">
+            <div className="icons">
+              <PlayArrow className="icon" />
+              <Add className="icon" />
+              <ThumbUpOutlined className="icon" />
+              <ThumbDownOutlined className="icon" />
+            </div>
+            <div className="itemInfoTop">
+              <span>1 hour 14 mins</span>
+              <span className="limit">+16</span>
+              <span>2013</span>
+            </div>
+            <div className="description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+              possimus ipsa corporis.
+            </div>
+            <div className="genre">Action, Anime, Fantasy</div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
