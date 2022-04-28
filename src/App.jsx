9f0b8f3beed9ae53944c29190c6de9c1774de432
TamikoSRN <thomasereno1@gmail.com>
2022-04-28 05:16:56 -0300

    Front-end finished, there are still some visual bugs on the Home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 import Register from './components/register/Register'
 import Home from './pages/home/Home'
 import Watch from './pages/watch/Watch'
+import Login from './components/login/Login'
 import "./App.scss"
 
 const App = () => {
   return (
-    <Register />
+    <Home />
   )
 }
 
