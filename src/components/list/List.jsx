33897fe6744172a77288a8c99ec096e2acc94ea1
TamikoSRN <thomasereno1@gmail.com>
2022-04-28 02:42:57 -0300

    item list finished

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,25 +1,35 @@
-import ListItem from "../listItem/ListItem";
-import {
-  ArrowBackIosOutlined,
-  ArrowForwardIosOutlined,
-} from "@material-ui/icons";
+import { useState, useRef } from "react";
+import { ArrowBackIosOutlined, ArrowForwardIosOutlined } from "@material-ui/icons";
 
+import ListItem from "../listItem/ListItem";
 import "./List.scss";
 
 const List = () => {
+
+  const [isMoved, setIsMoved] = useState(false)
+  const [slideNumber, setSlideNumber] = useState(0)
+
+  const listRef = useRef()
+
+  const handleClick = (direction) => {
+    setIsMoved(true)
+    let distance = listRef.current.getBoundingClientRect().x - 50
+    if (direction === "left" && slideNumber > 0) {
+      setSlideNumber(slideNumber - 1)
+      listRef.current.style.transform = `translateX(${230 + distance}px)`
+    }
+    if (direction === "right" && slideNumber < 5) {
+      setSlideNumber(slideNumber + 1)
+      listRef.current.style.transform = `translateX(${-230 + distance}px`
+    }
+  }
+
   return (
     <div className="list">
       <span className="listTitle">Continue to watch </span>
       <div className="wrapper">
-        <ArrowBackIosOutlined className="sliderArrow left" onClick={() => handleClick(left)} />
-        <div className="container">
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
+        <ArrowBackIosOutlined className="sliderArrow left" onClick={() => handleClick("left")} style={{display: !isMoved && "none"}} />
+        <div className="container" ref={listRef}>
           <ListItem />
           <ListItem />
           <ListItem />
@@ -31,7 +41,7 @@ const List = () => {
           <ListItem />
           <ListItem />
         </div>
-        <ArrowForwardIosOutlined className="sliderArrow right" onClick={() => handleClick(right)} />
+        <ArrowForwardIosOutlined className="sliderArrow right" onClick={() => handleClick("right")} />
       </div>
     </div>
   );
--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -1,11 +1,23 @@
-import "./ListItem.scss"
+import { Add, PlayArrow, ThumbDownOutlined, ThumbUpOutlined } from "@material-ui/icons";
+import "./ListItem.scss";
 
 const ListItem = () => {
   return (
-      <div className="listItem">
-          item
+    <div className="listItem">
+      <img
+        src="https://occ-0-2794-2219.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABWBSIsuEE08Fly6ugJ4krgHGBlajIpZ8jliwQ1QR8jvyZbNLkq3UXLLVzyIJdoED_xNum5pEDY9R6bWTjeWs-2ZdgZvpfs3bgK4VPfigQtJMVnWMDSg38dHusm-cfQ.jpg?r=5d0"
+        alt=""
+      />
+      <div className="itemInfo">
+        <div className="icons">
+          <PlayArrow />
+          <Add />
+          <ThumbUpOutlined />
+          <ThumbDownOutlined />
+        </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default ListItem
+export default ListItem;
